238f1c956927dfe98c08573452a9c947d9809221
Sanjula <sanjula8@gmail.com>
2020-03-20 19:50:02 -0500

    added nav bar to nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon" />
+			</button>
+		</nav>
+	);
+}
+
+export default Nav;
