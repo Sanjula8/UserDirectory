72669bb189607c33c7232a56ec92679f1a20d1ab
Sanjula <sanjula8@gmail.com>
2020-03-22 19:03:18 -0500

    added table, added consts for data area

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -6,40 +6,55 @@ import "../styles/DataArea.css";
 import DataAreaContext from "../utils/DataAreaContext";
 
 
-class DataArea extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			users: [{}],
-			order: "descend",
-			filteredUsers: [{}]
-		};
-	}
-	componentDidMount() {
-		//Api Call
-	}
-	render() {
-		return (
-			<>
-				<Nav />
-				<div>
-					<DataTable />
-				</div>
-			</>
-		);
-	}
-}
+const DataArea = () => {
+    const [developerState, setDeveloperState] = useState({
+      users: [],
+      order: "descend",
+      filteredUsers: [],
+      headings: [
+        { name: "Image", width: "10%", order: "descend" },
+        { name: "name", width: "10%", order: "descend" },
+        { name: "phone", width: "20%", order: "descend" },
+        { name: "email", width: "20%", order: "descend" },
+        { name: "dob", width: "10%", order: "descend" }
+      ]
+    });
+
+    
+// class DataArea extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			users: [{}],
+// 			order: "descend",
+// 			filteredUsers: [{}]
+// 		};
+// 	}
+// 	componentDidMount() {
+// 		//Api Call
+// 	}
+// 	render() {
+// 		return (
+// 			<>
+// 				<Nav />
+// 				<div>
+// 					<DataTable />
+// 				</div>
+// 			</>
+// 		);
+// 	}
+// }
 
 // Hooks:
-function DataArea2 {
-const [order, setOrder] = useState("descend")
-useEffect(() => {
-    //API Call
-    return () => {
+// function DataArea2 {
+// const [order, setOrder] = useState("descend")
+// useEffect(() => {
+//     //API Call
+//     return () => {
 
-    }
-}, [])
+//     }
+// }, [])
 
-}
+// }
 
 export default DataArea;
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import DataBody from "./DataBody";
-import { render } from "@testing-library/react";
-//Header Names
-//DoB
-//Image
-//Names
+import "../styles/DataTable.css";
+import DataAreaContext from "../utils/DataAreaContext";
 
-function DataTable(props) {
+const DataTable = () => {
+	const context = useContext(DataAreaContext);
+
 	return (
-		<div>
-			<table>
+		<div className="datatable mt-5">
+			<table
+				id="table"
+				className="table table-striped table-hover table-condensed"
+			>
 				<thead>
-					<th>Image</th>
-					<th>Name</th>
+					<tr>
+						{context.developerState.headings.map(
+							({ name, width }) => {
+								return (
+									<th
+										className="col"
+										key={name}
+										style={{ width }}
+										onClick={() => {
+											context.handleSort(name);
+										}}
+									>
+										{name}
+										<span className="pointer"></span>
+									</th>
+								);
+							}
+						)}
+					</tr>
 				</thead>
-				<DataBody users={props.users} />
+
+				<DataBody />
 			</table>
 		</div>
 	);
-}
+};
 
 export default DataTable;
